01f6d47a75ca78a6691d5380aba48e125fa07699
Michael Kornblum <michael@pop-os.localdomain>
2021-08-19 22:38:11 -0500

    remove age restriction, add class and static method

--- object-oriented/first-class.py
+++ object-oriented/first-class.py
@@ -1,9 +1,8 @@
 class PlayerCharacter():
     membership = True
     def __init__(self, name="anon", age=0):
-        if age > 18:
-            self.name = name
-            self.age = age
+        self.name = name
+        self.age = age
 
     def run(self):
         return f"{self.name} is running."
@@ -11,9 +10,22 @@ class PlayerCharacter():
     def shout(self):
         return f"My name is {self.name}!!!"
 
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls('Teddy', num1 + num2)
+
+    @staticmethod
+    def adding_things2(num1, num2):
+        return num1 + num2
+
 # player_one = PlayerCharacter('Michael', 50)
-player_two = PlayerCharacter('Roosevelt', 15)
+# player_two = PlayerCharacter('Roosevelt', 15)
 # print(player_one.name, player_one.age)
 # print(player_two.name, player_two.age)
-print(player_two.run())
+# print(player_two.run())
+
+# print(PlayerCharacter.adding_things(2, 3))
+
+player3 = PlayerCharacter.adding_things(2, 3)
+print(player3.age)
 
